a0bda92a024055dd300fdf050bf7f62dc25b18c9
Lars Toenning <dev@ltoenning.de>
2021-09-20 20:10:10 +0200

    [emacs] git-gutter: Use thin space

--- emacs/git.el
+++ emacs/git.el
@@ -9,9 +9,9 @@
 (use-package git-gutter)
 (global-git-gutter-mode +1)
 (custom-set-variables
- '(git-gutter:modified-sign " ")
- '(git-gutter:added-sign " ")
- '(git-gutter:deleted-sign " "))
+ '(git-gutter:modified-sign " ")
+ '(git-gutter:added-sign " ")
+ '(git-gutter:deleted-sign " "))
 (set-face-background 'git-gutter:modified "yellow")
 (set-face-background 'git-gutter:added "green")
 (set-face-background 'git-gutter:deleted "red")
