ffe194ca9f75284bc230e2b3d64922715d2b8a11
Lars Toenning <dev@ltoenning.de>
2021-09-25 10:35:08 +0200

    [emacs] Add straight

--- emacs/general.el
+++ emacs/general.el
@@ -32,6 +32,21 @@
 			("org" . "https://orgmode.org/elpa/")
 			("elpa". "https://elpa.gnu.org/packages/")))
 
+
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (package-initialize)
 (unless package-archive-contents
 (package-refresh-contents))
